5a8501ed203e3a2a09b2c9d84e62bd52f600c49f
Vikas Kumar <imvikaskumar99@gmail.com>
2022-07-30 10:33:19 +0530

    Task 3

---
+++ Python/Python Basic Task 3/integers_come_in_all_sizes.py
@@ -0,0 +1,4 @@
+a, b, c, d = int(input()), int(input()), int(input()), int(input())
+a_power_b = a ** b
+c_power_d = c ** d
+print(a_power_b + c_power_d)
---
+++ Python/Python Basic Task 3/polar_coordinates.py
@@ -0,0 +1,5 @@
+import cmath;
+num = complex(input())
+
+print(cmath.polar(num)[0])
+print(cmath.polar(num)[1])
