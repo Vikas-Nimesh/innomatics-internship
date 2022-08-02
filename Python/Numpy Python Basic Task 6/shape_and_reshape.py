4c3f9b072c6dcabd5ca0275d9806ae181ad97509
Vikas Kumar <imvikaskumar99@gmail.com>
2022-08-02 16:00:43 +0530

    Task 6

---
+++ Python/Numpy Python Basic Task 6/shape_and_reshape.py
@@ -0,0 +1,6 @@
+import numpy
+arr = input().strip().split(' ')
+arr_new = numpy.reshape(arr,(3,3))
+print(numpy.array(arr_new,int))
+
+
---
+++ Python/Numpy Python Basic Task 6/zeros_and_ones.py
@@ -0,0 +1,5 @@
+import numpy
+nums = tuple(map(int, input().split()))
+print (numpy.zeros(nums, int))
+print (numpy.ones(nums, int))
+
