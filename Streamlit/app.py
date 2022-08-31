96b3e7eba9147a89bf3139156e53c9c9ba3a2f93
Vikas Kumar <imvikaskumar99@gmail.com>
2022-08-31 16:11:18 +0530

    streamlit sample app

---
+++ Streamlit/app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title('Welcome to my Application')
+
+st.header('This is header')
+
+st.subheader('This is sub header')
+
+st.markdown('This is markdown')
+
+st.text('This is text')
+
+code = "print('Hello World')"
+
+st.code(code)
